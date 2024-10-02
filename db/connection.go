d933c438f5a15d541764adf9cb9d8c12f448efce
Marvin Seb <msebcu@gmail.com>
2024-10-02 01:04:59 -0600

    Implementación variables de entorno

--- db/connection.go
+++ db/connection.go
@@ -1,16 +1,29 @@
 package db
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DSN = "host=localhost user=geo password=geocompass dbname=geodb port=5432"
 var DB *gorm.DB
 
 func DBConnection() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var DSN = fmt.Sprintf("host=%s user=%s password=%s dbname=%s",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DATABASE"))
+
 	var error error
 	DB, error = gorm.Open(postgres.Open(DSN), &gorm.Config{})
 	if error != nil {
